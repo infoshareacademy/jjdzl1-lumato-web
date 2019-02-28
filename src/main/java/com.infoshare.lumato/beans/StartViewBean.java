40edd85d412e589f4e9b03b98564741bbe0b61fc
Łukasz Grymulski <lukasz.grymulski@gmail.com>
2019-02-28 03:18:31 +0100

    add second card

--- src/main/java/com.infoshare.lumato/beans/StartViewBean.java
+++ src/main/java/com.infoshare.lumato/beans/StartViewBean.java
@@ -1,5 +1,7 @@
 package com.infoshare.lumato.beans;
 
+import com.infoshare.lumato.dao.CarDAO;
+import com.infoshare.lumato.dao.FuelCostsDAO;
 import com.infoshare.lumato.dao.UserDAO;
 
 import javax.enterprise.context.RequestScoped;
@@ -13,12 +15,22 @@ public class StartViewBean {
     @Inject
     UserDAO userDAO;
 
+    @Inject
+    CarDAO carDAO;
+
+    @Inject
+    FuelCostsDAO fuelCostsDAO;
+
     public int countAllUsers(){
         return userDAO.countAllRecords("users");
     }
 
     public int countAllCars(){
-        return userDAO.countAllRecords("cars");
+        return carDAO.countAllRecords("cars");
+    }
+
+    public double calculateAverageFuelCost(String fuelType){
+        return fuelCostsDAO.calculateAverageFuelCost(fuelType);
     }
 
 }
--- src/main/java/com.infoshare.lumato/dao/FuelCostsDAO.java
+++ src/main/java/com.infoshare.lumato/dao/FuelCostsDAO.java
@@ -2,9 +2,26 @@ package com.infoshare.lumato.dao;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 @Named
 @RequestScoped
 public class FuelCostsDAO extends CommonDAO {
 
+    public double calculateAverageFuelCost(String fuelType){
+        double averageFuelCost = 0;
+        try {
+            String sql = "SELECT AVG(priceperliter) AS averageFuelCost FROM fuelcosts WHERE typeoffuel=?";
+            PreparedStatement myStmt = myConn.getConnection().prepareStatement(sql);
+            myStmt.setString(1, fuelType);
+            ResultSet resultSet = myStmt.executeQuery();
+            resultSet.next();
+            averageFuelCost = resultSet.getInt("averageFuelCost");
+        } catch (Exception exc) {
+            System.out.println("Cannot count average fuel cost!");
+            exc.printStackTrace();
+        }
+        return averageFuelCost;
+    }
 }
