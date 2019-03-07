2c0911354d457d1fdeaef236a871bb9e373a6402
Szakalakazo <tomek.starzak@gmail.com>
2019-03-07 10:08:40 +0100

    Add commit before messing around with datePicker

--- src/main/java/com.infoshare.lumato/services/CalendarService.java
+++ src/main/java/com.infoshare.lumato/services/CalendarService.java
@@ -10,11 +10,12 @@ public class CalendarService {
     }
 
     public static Calendar returnCalendarDate(String input) {
-         Calendar calendar = new GregorianCalendar();
+        Calendar calendar = new GregorianCalendar();
 
         String regex = "\\d{4}-\\d{2}-\\d{2}";
         if (input.matches(regex)) {
             String[] dateArray = input.split("-");
+//            "(^(((0[1-9]|1[0-9]|2[0-8])[-](0[1-9]|1[012]))|((29|30|31)[-](0[13578]|1[02]))|((29|30)[-](0[4,6,9]|11)))[-](19|[2-9][0-9])\\d\\d$)|(^29[-]02[-](19|[2-9][0-9])(00|04|08|12|16|20|24|28|32|36|40|44|48|52|56|60|64|68|72|76|80|84|88|92|96)$)\n"
 
             calendar.set(Calendar.YEAR, Integer.valueOf(dateArray[0]));
             calendar.set(Calendar.MONTH, Integer.valueOf(dateArray[1]));
