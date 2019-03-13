f23a751c35449d663276c8feddbad59a882fa069
Łukasz Grymulski <lukasz.grymulski@gmail.com>
2019-03-13 15:11:15 +0100

    add other sorting + style

--- src/main/java/com.infoshare.lumato/beans/UserDataBean.java
+++ src/main/java/com.infoshare.lumato/beans/UserDataBean.java
@@ -4,10 +4,9 @@ import com.infoshare.lumato.models.Car;
 import com.infoshare.lumato.models.FuelCosts;
 import com.infoshare.lumato.services.CarsService;
 import com.infoshare.lumato.services.FuelsCostsService;
-import com.infoshare.lumato.utils.FuelCostComparatorByDate;
 
 import javax.annotation.PostConstruct;
-import javax.faces.view.ViewScoped;
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
@@ -15,7 +14,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-@ViewScoped
+@RequestScoped
 @Named("userDataBean")
 public class UserDataBean implements Serializable {
 
@@ -53,14 +52,30 @@ public class UserDataBean implements Serializable {
     public void construct() {
         loadFuelCostList();
         loadCars();
-        sortById();
+        sortByDate();
     }
 
     public void sortByDate(){
+        Collections.sort(this.fuelCostsList, Comparator.comparing(FuelCosts::getDate));
+    }
+
+    public void sortByDateReversed(){
+        Collections.sort(this.fuelCostsList, Comparator.comparing(FuelCosts::getDate).reversed());
+    }
+
+    public void sortByCarIdAndThenByDate(){
         Collections.sort(this.fuelCostsList, Comparator.comparing(FuelCosts::getIdCar).thenComparing(FuelCosts::getDate));
     }
 
     public void sortById() {
         Collections.sort(this.fuelCostsList, Comparator.comparing(FuelCosts::getId));
+    }
+
+    public String getGetCarAsString(Integer idOfCar) {
+        Car car = carList.stream()
+                        .filter(c -> c.getCarId() == idOfCar)
+                        .findFirst()
+                        .get();
+        return car.getBrand() + " " + car.getModel() + " [" + car.getRegPlate() + "]";
     }
 }
