e1c4268c610a950ba8a1234e5676549b5b362fb2
Łukasz Grymulski <lukasz.grymulski@gmail.com>
2019-03-16 16:05:03 +0100

    remove unused file

--- src/main/java/com.infoshare.lumato/dao/ExtraCostsDAO.java
+++
@@ -1,9 +0,0 @@
-package com.infoshare.lumato.dao;
-
-import javax.enterprise.context.RequestScoped;
-import javax.inject.Named;
-
-@Named
-@RequestScoped
-public class ExtraCostsDAO extends CommonDAO {
-}
