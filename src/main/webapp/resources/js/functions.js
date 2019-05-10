d002d766bdd786623db49fe5647094f98d7367bf
Łukasz Grymulski <lukasz.grymulski@gmail.com>
2019-05-10 16:14:41 +0200

    make navbar links highlight when inside

---
+++ src/main/webapp/resources/js/functions.js
@@ -0,0 +1,41 @@
+window.onload = function(e) {
+    updateNavbarLinkHighlighted();
+}
+
+var path;
+var active;
+var newActiveItemId;
+
+function updateNavbarLinkHighlighted(){
+    active = document.querySelector(".active");
+    if (active != null) {
+        removeActive();
+    }
+    highlightNew();
+}
+
+function removeActive(){
+    active.classList.remove("active");
+}
+
+function highlightNew() {
+    path = location.pathname.substring(1);
+
+    newActiveItemId = pathMatches(/start/) ? "nav-home" :
+    pathMatches(/cars-input|user-edit|user-change-password|user-delete/ ) ? "nav-account" :
+    pathMatches(/fuel-input|cost-input/) ? "nav-register-data" :
+    pathMatches(/charts/) ? "nav-stats" :
+    pathMatches(/contact/) ? "nav-contact" : "";
+
+    var element = document.getElementById(newActiveItemId);
+    if (element != null) {
+        element.classList.add("active");
+    }
+}
+
+function pathMatches(regex) {
+    if (path.match(regex)) {
+        return true;
+    }
+    return false;
+}
